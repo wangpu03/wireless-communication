bd21bae57305e2d4d1fe04331892465debadafb3
Pu Wang <wangpu03@gmail.com>
2019-05-06 10:56:55 -0400

    update ray_model

--- MIMO-OFDM-Wireless-Communications-with-MATLAB/Ray_model.m
+++ MIMO-OFDM-Wireless-Communications-with-MATLAB/Ray_model.m
@@ -3,7 +3,8 @@ function H=Ray_model(L)
 %  Input : L  : # of channel realization
 %  Output: H  : Channel vector
 
-%MIMO-OFDM Wireless Communications with MATLAB¢ç   Yong Soo Cho, Jaekwon Kim, Won Young Yang and Chung G. Kang
+%MIMO-OFDM Wireless Communications with MATLAB¢ç   Yong Soo Cho, Jaekwon Kim, 
+% Won Young Yang and Chung G. Kang
 %2010 John Wiley & Sons (Asia) Pte Ltd
 
 H = (randn(1,L)+j*randn(1,L))/sqrt(2);
